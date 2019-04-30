4a7e212fcfca91239b3838c5ba48944741eb7243
newinker <newinker@isunnan.com>
2019-04-30 15:54:39 +0800

    修改SECRET_KEY设置。

--- sayhello/settings.py
+++ sayhello/settings.py
@@ -20,6 +20,6 @@ else:
 
 dev_db = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'secret string')
+SECRET_KEY = os.getenv('SECRET_KEY', 'dev')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', dev_db)
