9c691efa537bb3c3b6f8660a782c819a84c40a9c
newinker <newinker@isunnan.com>
2019-05-06 20:45:03 +0800

    Revert "消息发布时间，用Timestamp类型代替Datetime，以支持跨时区。"
    This reverts commit d3112fe4

--- sayhello/models.py
+++ sayhello/models.py
@@ -15,6 +15,4 @@ class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
     body = db.Column(db.String(200))
-    # timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
-    timestamp = db.Column(db.TIMESTAMP(True), server_default=db.text('NOW()'))
-    # timestamp = db.Column(db.DateTime(True), server_default=db.text('NOW()'))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
